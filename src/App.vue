dfee913e1dd5ae539e955d9d250bd8e3a8cd4889
keisuke watanabe <kskwtnb93@gmail.com>
2022-07-18 19:55:53 +0900

    Layout調整

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" app>
+        <SidebarComponent />
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-app-bar>
+
+      <v-main>
+        <!--  -->
+        <router-view />
+      </v-main>
+    </v-app>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import SidebarComponent from "@/components/Layouts/SidebarComponent";
+
+export default {
+  components: {
+    SidebarComponent,
+  },
+  data: () => ({
+    drawer: null,
+    //  selectedItem: 0,
+    //  links: [
+    //    ["mdi-home", "Home", "/"],
+    //    ["mdi-send", "About", "/about"],
+    //  ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Layouts/SidebarComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> John Leider </v-list-item-title>
+          <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list nav dense>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item
+          v-for="[icon, text, to] in links"
+          :key="icon"
+          :to="to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectedItem: 0,
+    links: [
+      ["mdi-home", "Home", "/"],
+      ["mdi-send", "About", "/about"],
+    ],
+  }),
+};
+</script>
