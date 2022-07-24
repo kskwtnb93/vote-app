72fce3a25a449bee73916269e50c3adfec2d107a
keisuke watanabe <kskwtnb93@gmail.com>
2022-07-24 09:04:41 +0900

    ユーザーリスト表示テスト

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-list class="user-list" subheader>
+      <v-list-item
+        class="user-list__item"
+        v-for="user in users"
+        :key="user.uid"
+      >
+        <v-list-item-avatar>
+          <v-img
+            :alt="`${user.displayName} avatar`"
+            src="https://cdn.vuetifyjs.com/images/lists/2.jpg"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="user.displayName"></v-list-item-title>
+        </v-list-item-content>
+
+        <!-- <v-list-item-icon>
+          <v-icon> mdi-message-outline </v-icon>
+        </v-list-item-icon> -->
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import firebase from "@/firebase/firebase";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data: () => ({
+    users: [],
+  }),
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      this.users = [];
+
+      // ドキュメント取得
+      // ref = 参照的なニュアンスの意味
+      const usersRef = firebase.firestore().collection("users");
+      const snapshot = await usersRef.get();
+      console.log("snapshot", snapshot);
+
+      // snapshotがオブジェクト型なのでmapではなくforEach使わないと展開できない
+      // snapshot.forEach((doc) => {
+      //   // data()メソッド使わないと見れない
+      //   console.log(doc.data());
+      // });
+
+      snapshot.docs.map((doc) => {
+        // docsであれば配列として受け取れるのでmapを使って展開できる
+        const data = { ...doc.data() };
+        data.id = doc.id;
+
+        // data()メソッド使わないと見れない
+        console.log(data);
+        this.users.push(data);
+      });
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.user-list {
+  &__item {
+    border-bottom: thin solid rgba(0, 0, 0, 0.12);
+  }
+}
+</style>
